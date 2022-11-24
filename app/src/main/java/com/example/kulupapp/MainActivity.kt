6ed0da646d232482e164fff5ad5f435ac579ad46
artun <artunankara@gmail.com>
2022-11-24 17:04:05 +0300

    login and register logic

--- app/src/main/java/com/example/kulupapp/MainActivity.kt
+++ app/src/main/java/com/example/kulupapp/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.kulupapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Switch
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -11,11 +15,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_panel)
+
+        //Firebase setting
+        var authL = FirebaseAuth.getInstance()
+
+        //swich activity
         val sw1 = findViewById<Switch>(R.id.switch2)
         sw1.setOnClickListener{
             val intent = Intent(this,RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        //login buttons
+        val btnID = findViewById<Button>(R.id.btn_login)//button id area
+        val stdnumberID = findViewById<EditText>(R.id.et_stdNumber)
+        val passwordID = findViewById<EditText>(R.id.et_password)
+
+        val stdnumber = stdnumberID.text//text value set area
+        val pas = passwordID.text
+
+        btnID.setOnClickListener {//Button logic
+            if(stdnumber.isEmpty()){
+                stdnumberID.error = "Öğrenci numarasını giriniz"
+            }
+            else if(pas.isEmpty()){
+                passwordID.error = "Şifrenizi giriniz"
+            }
+            else{
+
+            }
+        }
+
+
+
         val database = Firebase.database
         val myRef = database.getReference("message")
 
--- app/src/main/java/com/example/kulupapp/RegisterActivity.kt
+++ app/src/main/java/com/example/kulupapp/RegisterActivity.kt
@@ -7,11 +7,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Switch
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_panel)
+        //Firebase setting
+        var authR = FirebaseAuth.getInstance()
+
         //swich activity
         val sw1 = findViewById<Switch>(R.id.switch2)
 
@@ -19,7 +23,7 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
-        //register button
+        //register buttons
         val btnID = findViewById<Button>(R.id.btn_register)//button id area
         val stdnumberID = findViewById<EditText>(R.id.et_stdNumber)
         val nameID = findViewById<EditText>(R.id.et_name)
@@ -33,9 +37,31 @@ class RegisterActivity : AppCompatActivity() {
         val pas = passwordID.text
         val repas = repaswordID.text
 
-        btnID.setOnClickListener {
+        btnID.setOnClickListener { //Button logic
             if (stdnumber.isEmpty()){
-                stdnumberID.shadowColor
+                stdnumberID.error = "Öğrenci numarasını giriniz"
+            }
+            else if (name.isEmpty()){
+                nameID.error = "İsminizi giriniz giriniz"
+            }
+            else if (email.isEmpty()){
+                emailID.error = "Öğrenci numaranızı giriniz"
+            }
+            else if (pas.isEmpty()){
+                passwordID.error = "Şifreniz giriniz giriniz"
+            }
+            else if (repas.isEmpty()){
+                repaswordID.error = "Şifre tekrarınızı giriniz"
+            }
+            else if (pas != repas){
+                Toast.makeText(this, "Şifre ve şifre terarı aynı olmalıdır", Toast.LENGTH_SHORT).show()
+                repaswordID.error = "Şifre ve şifre terarı aynı olmalıdır"
+            }
+            else{
+                authR.createUserWithEmailAndPassword(stdnumber.toString(),pas.toString()).addOnCompleteListener {  }
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+
             }
         }
 
